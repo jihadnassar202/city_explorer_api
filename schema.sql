19e0fb513bed35030e094dbc2771dbad13f2cbca
jihadnassar <jihad.nassar202@gmail.com>
2026-01-21 10:58:14 +0300

    used redis to cache the data so it doesnt generate a new request from the api

---
+++ schema.sql
@@ -0,0 +1,22 @@
+ CREATE TABLE IF NOT EXISTS weather (
+    id SERIAL PRIMARY KEY,
+    city VARCHAR(255) NOT NULL,
+    country_code VARCHAR(255) NOT NULL,
+    data JSONB NOT NULL
+ );
+
+ CREATE TABLE IF NOT EXISTS location (
+    id SERIAL PRIMARY KEY,
+    city VARCHAR(255) NOT NULL,
+    latitude DECIMAL(10, 8) NOT NULL,
+    longitude DECIMAL(11, 8) NOT NULL
+ );
+
+ CREATE TABLE IF NOT EXISTS parks (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    cost DECIMAL(10, 2) NOT NULL,
+    description TEXT NOT NULL,
+    url VARCHAR(255) NOT NULL
+ );
--- server/server.js
+++ server/server.js
@@ -6,8 +6,10 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
-
-//WeatherAPI class
+const redis = require('redis');
+const redisClient = redis.createClient();
+redisClient.connect().catch(console.error);
+const DEFAULT_EXPIRATION = 60 * 60 * 24;
 
 function mustenv(name) {
     if (!process.env[name]) {
@@ -21,28 +23,37 @@ const locationAPI = mustenv('GEOCODE_API_KEY');
 const parkAPI = mustenv('PARK_API_KEY');
 
 app.get('/weather/:city_name', async (req, res) => {
-
-    const { city_name } = req.params;
-    const weather = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city_name}&key=${weatherAPI}`);
-    //check if the weather is found
-    if (weather.status !== 200) {
-        res.status(404).json({ error: 'Weather not found' });
-        return;
+    const city_name = req.params.city_name.trim().toLowerCase();
+    const cachekey = `weather:${city_name}`;
+    try {
+        const cachedData = await redisClient.get(cachekey);
+        if (cachedData) {
+            return res.json(JSON.parse(cachedData));
+        }
+        const weather = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city_name}&key=${weatherAPI}`);
+        if (weather.status !== 200) {
+            res.status(404).json({ error: 'Weather not found' });
+            return;
+        }
+        const weatherData = await weather.json();
+        const formattedWeatherData = {
+            "city": weatherData.city_name,
+            "country_code": weatherData.country_code,
+            "data": weatherData.data.map(item => ({
+                "date": item.valid_date,
+                "low_temp": item.low_temp,
+                "high_temp": item.high_temp,
+                "description": item.weather.description,
+            }))
+        }
+        await redisClient.setEx(cachekey, DEFAULT_EXPIRATION, JSON.stringify(formattedWeatherData));
+        res.json(formattedWeatherData);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
-    //return the weather data in the format of the weather.json file
-    const weatherData = await weather.json();
-    res.json({
-        "city": weatherData.city_name,
-        "country_code": weatherData.country_code,
-        "data": weatherData.data.map(item => ({
-            "date": item.valid_date,
-            "low_temp": item.low_temp,
-            "high_temp": item.high_temp,
-            "description": item.weather.description,
-        }))
-    });
-
 });
+
 app.get('/location/:display_name', async (req, res) => {
     const { display_name } = req.params;
     const location = await fetch(`https://us1.locationiq.com/v1/search?key=${locationAPI}&q=${display_name}&format=json`);
